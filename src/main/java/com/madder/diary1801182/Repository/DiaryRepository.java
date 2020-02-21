0337be8ac3b2bf5880511ed3575700ad89278c81
hoashi <akane.hoashi@outlook.jp>
2020-02-22 02:05:15 +0900

    日誌チェック機能追加

--- src/main/java/com/madder/diary1801182/Repository/DiaryRepository.java
+++ src/main/java/com/madder/diary1801182/Repository/DiaryRepository.java
@@ -2,6 +2,7 @@ package com.madder.diary1801182.Repository;
 
 import com.madder.diary1801182.Dto.DiaryDto;
 import com.madder.diary1801182.Entity.Diary;
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -17,5 +18,10 @@ public interface DiaryRepository extends JpaRepository<Diary, Diary>{
 //    日誌一覧取得
     @Query("FROM Diary WHERE classCode = :classCode")
     public List<Diary> getDiaryList(@Param("classCode")  String classCode);
+
+//    日付の日誌がなければtrueを返す
+    @Query("SELECT CASE WHEN count(diaryCode) = 0 THEN true ELSE false END FROM Diary WHERE classCode = :classCode AND insertDate = :insertDate")
+    public boolean diaryExists(@Param("classCode") String classCode,@Param("insertDate") Date insertDate);
+
 
 }
