febbc2be779dd6d1f64b40e1235b09ec9cf1cf98
hoashi <akane.hoashi@outlook.jp>
2020-02-22 02:04:49 +0900

    Diaryテーブル初期値、autoIncrement設定、(完了、日誌入力)html追加

--- src/main/java/com/madder/diary1801182/Dto/DiaryDto.java
+++ src/main/java/com/madder/diary1801182/Dto/DiaryDto.java
@@ -1,20 +1,37 @@
 package com.madder.diary1801182.Dto;
 
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Tolerate;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
+@Builder
+
 public class DiaryDto implements Serializable {
 
-    private Date insertDate;
+    @NotNull
+    private String insertDate;
+    @NotNull
     private String classCode;
+    @NotNull
     private String studentId;
+    @NotNull
     private String goodPoint;
+    @NotNull
     private String badPoint;
+    @NotNull
     private String stdCom;
+    @NotNull
     private String tcrCom;
 
+    @Tolerate
+    DiaryDto(){
+
+    }
 }
--- src/main/java/com/madder/diary1801182/Entity/Diary.java
+++ src/main/java/com/madder/diary1801182/Entity/Diary.java
@@ -1,6 +1,8 @@
 package com.madder.diary1801182.Entity;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.Tolerate;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -9,12 +11,14 @@ import java.util.List;
 
 @Table(name = "diary")
 @Entity
+@Builder
 @Data
 public class Diary implements Serializable {
     private static final long serialVersionUID = 1L;
 
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "diary_code", insertable = false, nullable = false)
     private Integer diaryCode;
 
@@ -27,21 +31,28 @@ public class Diary implements Serializable {
     @Column(name = "student_id", nullable =false)
     private String studentId;
 
+    @Builder.Default
     @Column(name = "good_point")
     private String goodPoint = "NULL";
 
+    @Builder.Default
     @Column(name = "bad_point")
     private String badPoint = "NULL";
 
+    @Builder.Default
     @Column(name = "student_comment")
     private String studentComment = "NULL";
 
+    @Builder.Default
     @Column(name = "teacher_comment", nullable = false)
-    private String teacherComment;
+    private String teacherComment = "";
 
     @OneToMany(mappedBy = "classes")
     List<Class> classes;
 
     @OneToMany(mappedBy ="students")
     List<Student> students;
+
+    @Tolerate
+    Diary(){}
 }
