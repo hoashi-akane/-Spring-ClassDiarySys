c43ace7ee966055479b256c6a98c5903991a1bf8
hoashi <akane.hoashi@outlook.jp>
2021-01-03 04:05:45 +0900

    変更：認証関連のテストを内部クラスに移動

--- src/test/java/com/madder/diary1801182/Controller/LoginControllerTest.java
+++ src/test/java/com/madder/diary1801182/Controller/LoginControllerTest.java
@@ -1,40 +1,25 @@
 package com.madder.diary1801182.Controller;
 
 import com.madder.diary1801182.Dto.LoginInfoDto;
-import com.madder.diary1801182.Form.LoginForm;
-import com.madder.diary1801182.Repository.ClassRepository;
-import com.madder.diary1801182.Repository.CourseRepository;
-import com.madder.diary1801182.Repository.StudentRepository;
 import com.madder.diary1801182.Service.LoginService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyObject;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(LoginController.class)
 @DisplayName("LoginControllerのテスト")
@@ -56,26 +41,40 @@ public class LoginControllerTest {
                 .andExpect(status().isOk());
     }
 
-    @Test
-    public void authへのリクエスト() throws Exception {
+    @Nested
+    class Auth関連 {
 
-        LoginInfoDto loginInfoDto = LoginInfoDto.builder()
-                .userId("1")
-                .userName("")
-                .classCode("student.getClassCode()")
-                .className("myClass.getClassName()")
-                .courseCode("myClass.getCourseCode()")
-                .courseName("course.getCourseName()")
-                .build();
+//      ServiceのauthenticationメソッドからLoginInfoDtoが帰ってくればログイン成功、nullなら失敗
+        @Test
+        public void authへのリクエスト_ログイン失敗時にリダイレクトするか() throws Exception {
 
-        when(loginService.authentication(any())).thenReturn(loginInfoDto);
-        ResultActions accept = mockMvc.perform(post("/Auth")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("userId", "10")
-                .param("password", "password")
-                .characterEncoding("utf-8")
-        );
+            when(loginService.authentication(any())).thenReturn(null);
+            ResultActions accept = mockMvc.perform(post("/Auth")
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .param("userId", "10")
+                    .param("password", "password")
+                    .characterEncoding("utf-8")
+            );
+            accept.andExpect(status().is(301));
+            accept.andExpect(redirectedUrl("/Login"));
+        }
 
-        accept.andExpect(status().is(301));
+        @Test
+        public void authへのリクエスト_ログイン成功時にリダイレクトするか() throws Exception {
+
+            //      値がからでもログイン成功する
+            LoginInfoDto loginInfoDto = LoginInfoDto.builder()
+                    .build();
+            when(loginService.authentication(any())).thenReturn(loginInfoDto);
+
+            ResultActions accept = mockMvc.perform(post("/Auth")
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .param("userId", "10")
+                    .param("password", "password")
+                    .characterEncoding("utf-8")
+            );
+            accept.andExpect(status().is(301));
+            accept.andExpect(redirectedUrl("/Menu"));
+        }
     }
 }
