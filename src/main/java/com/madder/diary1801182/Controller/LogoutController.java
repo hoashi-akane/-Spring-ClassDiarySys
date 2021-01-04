965c705e0c94117d870bccc1ced2e82393a525da
hoashi <akane.hoashi@outlook.jp>
2021-01-04 17:43:07 +0900

    修正：リダイレクトを302から301で行うよう変更

--- src/main/java/com/madder/diary1801182/Controller/LogoutController.java
+++ src/main/java/com/madder/diary1801182/Controller/LogoutController.java
@@ -1,8 +1,10 @@
 package com.madder.diary1801182.Controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpSession;
 
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpSession;
 public class LogoutController {
 
     @GetMapping("Logout")
+    @ResponseStatus(value= HttpStatus.MOVED_PERMANENTLY)
     public String logout(HttpSession session){
 
         session.invalidate();
